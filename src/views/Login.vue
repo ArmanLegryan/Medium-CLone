1fc25e9574f47931ca4083b491acc290af98811d
legryan <legryan@gmail.com>
2021-03-05 08:28:26 +0400

    Created Login.vue, fuctional for login to the site

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,24 +3,15 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-6  col-xs-12" style="margin: 0 auto">
-          <h1 class="text-xs-center">Sign up</h1>
+          <h1 class="text-xs-center">Sign in</h1>
           <p class="text-xs-center">
-            <router-link :to="{name: 'login'}">Have an account?</router-link>
+            <router-link :to="{name: 'register'}">Need an account?</router-link>
           </p>
           <mcv-validation-errors
             v-if="validationErrors"
             :validation-errors="validationErrors"
           />
           <form @submit.prevent="onSubmit">
-            <fieldset class="form-group">
-              <input
-                type="text"
-                class="form-control form-control-lg"
-                placeholder="Username"
-                v-model="username"
-              />
-            </fieldset>
-
             <fieldset class="form-group">
               <input
                 type="text"
@@ -43,7 +34,7 @@
               class="btn btn-lg btn-primary pull-xs-right"
               :disabled="isSubmitting"
             >
-              Sign Up
+              Sign In
             </button>
           </form>
         </div>
@@ -58,15 +49,14 @@ import McvValidationErrors from '@/components/ValidationErrors'
 import {actionTypes} from '@/store/modules/auth'
 
 export default {
-  name: 'McvRegister',
+  name: 'McvLogin',
   components: {
     McvValidationErrors
   },
   data() {
     return {
       email: '',
-      password: '',
-      username: ''
+      password: ''
     }
   },
   computed: {
@@ -77,15 +67,12 @@ export default {
   },
   methods: {
     onSubmit() {
-      console.log('Submitted form')
       this.$store
-        .dispatch(actionTypes.register, {
+        .dispatch(actionTypes.login, {
           email: this.email,
-          username: this.username,
           password: this.password
         })
-        .then(user => {
-          console.log('Success', user)
+        .then(() => {
           this.$router.push({name: 'home'})
         })
     }
